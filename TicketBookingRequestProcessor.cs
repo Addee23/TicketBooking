61128a8662a2d65ed17573bc23f321e270d7297b
Adina Moldovan <adina.moldovan.webd24jon@edu.tucsweden.se>
2025-01-30 15:58:15 +0100

    Version 2: Ett test passerar

--- TicketBookingRequestProcessor.cs
+++ TicketBookingRequestProcessor.cs
@@ -3,16 +3,27 @@ namespace TicketBookingCore
 {
     public class TicketBookingRequestProcessor
     {
+        private readonly ITicketBookingRepository _ticketBookingRepository;
         public TicketBookingRequestProcessor(ITicketBookingRepository ticketBookingRepository)
         {
+            _ticketBookingRepository = ticketBookingRepository;
         }
-
+        
         public TicketBookingResponse Book(TicketBookingRequest request)
         {
-            if (request is null)
-            {
+        
+           {
                 throw new ArgumentNullException(nameof(request));
             }
+
+            //kod för att spara i databasen
+            _ticketBookingRepository.Save(new TicketBooking
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email
+            });
+
             //refractor för att returnera en ny TicketBookingResponse
             return new TicketBookingResponse
             {
