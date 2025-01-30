fd1947dc94f01b9bfb9ea14553839423c822e908
Adina Moldovan <adina.moldovan.webd24jon@edu.tucsweden.se>
2025-01-30 16:52:13 +0100

    Version 3. Refaktorisering - Alla tester gröna

--- TicketBookingRequestProcessor.cs
+++ TicketBookingRequestProcessor.cs
@@ -1,31 +1,35 @@
-﻿
-namespace TicketBookingCore
+﻿namespace TicketBookingCore
 {
     public class TicketBookingRequestProcessor
     {
+        //suggesting that _ticketBookingRepository will hold a reference to an object that implements this interface.
         private readonly ITicketBookingRepository _ticketBookingRepository;
         public TicketBookingRequestProcessor(ITicketBookingRepository ticketBookingRepository)
         {
             _ticketBookingRepository = ticketBookingRepository;
         }
-        
+
         public TicketBookingResponse Book(TicketBookingRequest request)
         {
-        
-           {
+            if (request is null)
+            {
                 throw new ArgumentNullException(nameof(request));
             }
 
-            //kod för att spara i databasen
-            _ticketBookingRepository.Save(new TicketBooking
-            {
-                FirstName = request.FirstName,
-                LastName = request.LastName,
-                Email = request.Email
-            });
+            _ticketBookingRepository.Save(Create<TicketBooking>(request));
 
-            //refractor för att returnera en ny TicketBookingResponse
-            return new TicketBookingResponse
+            return Create<TicketBookingResponse>(request);
+        }
+        /// <summary>
+        /// This method creates a new instance of the specified type 
+        /// and sets the properties from the request object.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        private static T Create<T>(TicketBookingRequest request) where T : TicketBookingBase, new()
+        {
+            return new T
             {
                 FirstName = request.FirstName,
                 LastName = request.LastName,
